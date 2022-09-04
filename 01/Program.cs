d37e14c95cd4ec4ed76c826fc412405a99ec449a
max <matrixbyine.gmail.com>
2022-09-04 15:16:57 +0300

    Сумма элементов на нечетных позициях

---
+++ 01/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+
+int[] FillArray(int size)
+{
+   int[] array = new int[size];
+
+   for (int i = 0; i < size; i++)
+   {
+      array[i] = new Random().Next(-50, 50);
+      if (i != array.Length - 1) Console.Write($"{array[i]}, ");
+      else Console.WriteLine(array[i]);
+   }
+   return array;
+}
+
+int Sumnegpoz(int[] array)
+{
+   int sum = 0;
+   for (int i = 1; i < array.Length; i = i + 2) sum = sum + array[i];
+   return sum;
+}
+
+
+try
+{
+   Console.WriteLine("Введите число элементов массива");
+   int size = Convert.ToInt32(Console.ReadLine());
+   int sum = Sumnegpoz(FillArray(size));
+   Console.WriteLine($"Количество четных чисел: {sum}");
+}
+catch
+{
+   Console.WriteLine("Ошибка. Введите число!!");
+}
